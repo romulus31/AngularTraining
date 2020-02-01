826573a49bdaeef55adea1312ae3ccc3b1e0deb6
Romain <romain.fauconnier@yahoo.fr>
2020-02-01 10:25:14 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my awesome project !';
+
+  isAuth = false;
+  appareilOne = "Robot cuisine";
+  appareilTwo = "Machine a laver";
+  appareilThree = "Lave vaisselle";
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+
+  }
+
+  onAllumer(): void {
+
+  }
+
 }
---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+
+  @Input() appareilName: string;
+  appareilStatus: string = 'éteint';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getStatus() {
+    return this.appareilStatus;
+  }
+
+}
