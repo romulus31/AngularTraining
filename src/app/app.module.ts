95a4908d780a04004ae713e5dc31bcb8e673ab48
Romain <romain.fauconnier@yahoo.fr>
2020-02-02 16:43:18 -0500

    AuthGard with canActivate in appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,11 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { Routes, RouterModule } from '@angular/router';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { AuthGuard } from './services/auth-gard.service';
 
 const appRoutes: Routes = [
-  { path: 'appareils', component: AppareilViewComponent },
-  { path: 'appareils/:id', component: SingleAppareilComponent },
+  { path: 'appareils', canActivate:[AuthGuard], component: AppareilViewComponent },
+  { path: 'appareils/:id', canActivate:[AuthGuard], component: SingleAppareilComponent },
   { path: 'auth', component: AuthComponent },
   { path: '', component: AppareilViewComponent },
   { path: 'not-found', component: FourOhFourComponent },
@@ -40,7 +41,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes)
   ],
   providers: [AppareilService,
-     AuthService
+     AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
